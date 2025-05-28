1720f8a7f35c25feb7d9f857507ed239b7723d75
moonia <mounia.arjdal@epitech.eu>
2025-05-28 02:59:09 +0200

    refactor(profiler): clean & optimize CPU profiling & flamegraph generation

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"errors"
+	// "io"
 	"log"
 	"os"
 	"os/exec"
@@ -10,92 +11,102 @@ import (
 	"time"
 )
 
-func generateFlamegraph() {
-	var out bytes.Buffer
-	log.Println("Generating flamegraph...")
-	a := exec.Command("go", "tool", "pprof", "-raw", "./cpu.pprof")
-	b := exec.Command("/home/pol/downloads/FlameGraph/stackcollapse-go.pl")
-	c := exec.Command("/home/pol/downloads/FlameGraph/flamegraph.pl")
-	aOut, err := a.StdoutPipe()
+func main() {
+	err := profile(runApp, 100 * time.Second)
 	if err != nil {
-		log.Fatal("can't pipe a:", err)
+		log.Fatal("Profiling failed:", err)
 	}
-	b.Stdin = aOut
-	bOut, err := b.StdoutPipe()
-	if err != nil {
-		log.Fatal("can't pipe b:", err)
-	}
-	c.Stdin = bOut
-	c.Stdout = &out
-	if err := c.Start(); err != nil {
-		log.Fatal("can't start c: ", err)
-	}
-	if err := b.Start(); err != nil {
-		log.Fatal("can't start b: ", err)
-	}
-	if err := a.Run(); err != nil {
-		log.Fatal("can't run a: ", err)
-	}
-	if err := b.Wait(); err != nil {
-		log.Fatal("b failed: ", err)
-	}
-	if err := c.Wait(); err != nil {
-		log.Fatal("c failed: ", err)
-	}
-	if err := os.WriteFile("flamegraph.svg", out.Bytes(), 0644); err != nil {
-		log.Fatal("could not create flamegraph:", err)
-	}
-	log.Println("Flamegraph generated")
 }
 
-func profile(fn func(), t time.Duration) {
-	log.Println("Starting CPU Profiling...")
+func profile(fn func(), duration time.Duration) error {
+	log.Println("Starting CPU profiling...")
+
 	f, err := os.Create("cpu.pprof")
 	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
+		return err
 	}
 	defer f.Close()
 
 	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
+		return err
 	}
-
-	profilingDone := make(chan struct{})
 	fnDone := make(chan struct{})
+	
 	go func() {
-		select {
-		case <-time.After(t):
-			break
-		case <-fnDone:
-			time.Sleep(time.Second)
-			break
-		}
-		pprof.StopCPUProfile()
-		log.Println("CPU profiling stopped")
-		generateFlamegraph()
-		close(profilingDone)
+		fn()
+		close(fnDone)
 	}()
 
-	fn()
-	close(fnDone)
-	<-profilingDone
+	select {
+	case <-time.After(duration):
+		log.Println("Profiling duration reached.")
+	case <-fnDone:
+		log.Println("Function completed before timeout.")
+	}
+
+	pprof.StopCPUProfile()
+	log.Println("CPU profiling stopped.")
+	return generateFlamegraph("cpu.pprof", "flamegraph.svg")
 }
 
-func main() {
-	profile(runApp, 10*time.Second)
+func generateFlamegraph(profilePath, outputPath string) error {
+	log.Println("Generating flamegraph...")
+	cmdA := exec.Command("go", "tool", "pprof", "-raw", profilePath) // go tool pprof -raw cpu.pprof
+	cmdB := exec.Command("/Users/moonia/FlameGraph/stackcollapse-go.pl") // stackcollapse-go.pl
+	cmdC := exec.Command("/Users/moonia/FlameGraph/flamegraph.pl") // flamegraph.pl
+	aOut, err := cmdA.StdoutPipe()
+
+	if err != nil {
+		return errors.New("failed to get stdout from pprof: " + err.Error())
+	}
+	cmdB.Stdin = aOut
+
+	bOut, err := cmdB.StdoutPipe()
+	if err != nil {
+		return errors.New("failed to get stdout from stackcollapse: " + err.Error())
+	}
+	cmdC.Stdin = bOut
+
+	var flamegraph bytes.Buffer
+	cmdC.Stdout = &flamegraph
+
+	if err := cmdC.Start(); err != nil {
+		return errors.New("failed to start flamegraph.pl: " + err.Error())
+	}
+	if err := cmdB.Start(); err != nil {
+		return errors.New("failed to start stackcollapse-go.pl: " + err.Error())
+	}
+	if err := cmdA.Run(); err != nil {
+		return errors.New("failed to run go tool pprof: " + err.Error())
+	}
+	if err := cmdB.Wait(); err != nil {
+		return errors.New("stackcollapse-go.pl failed: " + err.Error())
+	}
+	if err := cmdC.Wait(); err != nil {
+		return errors.New("flamegraph.pl failed: " + err.Error())
+	}
+	if err := os.WriteFile(outputPath, flamegraph.Bytes(), 0644); err != nil {
+		return errors.New("failed to write flamegraph.svg: " + err.Error())
+	}
+	log.Println("Flamegraph generated at:", outputPath)
+	return nil
 }
 
 func runApp() {
-	fmt.Println(fib(30))
-	// for i := 0; i < 30; i++ {
-	// 	fmt.Println(fib(i))
-	// 	time.Sleep(100 * time.Millisecond)
-	// }
+	start := time.Now()
+	for time.Since(start) < 10*time.Second {
+		_ = fib(10)
+	}
 }
 
+// O(n) for iterative version
 func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
-	return fib(n-1) + fib(n-2)
+	a, b := 0, 1
+	for i := 2; i <= n; i++ {
+		a, b = b, a+b
+	}
+	return b
 }
